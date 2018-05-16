666bc265a8860c749db2d9e87be0e5f1e21ad659
Victor fernandez <victor.fernandezgil@educa.madrid.org>
2018-05-16 19:52:57 +0200

    initial

---
+++ src/examen/Examen.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen;
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+
+public class Examen {
+    
+    // Ejercicio1 
+    public double calcularCosteTotalMateriales(String filename) {
+        
+    }
+    
+    // Ejercicio 2
+    public int obtenerLosMasHabladores(String fileNameEntrada, String fileNameSalida) {
+        
+    }
+    
+    // Ejercicio 3
+    public double radioMedio(int radioMinimo) {
+        
+    }
+    
+}
